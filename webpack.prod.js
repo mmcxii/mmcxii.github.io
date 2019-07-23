34c83c550ca68e6db4fe755bb7af997da164e862
Nich Secord <nicholassecord@gmail.com>
2019-07-23 15:29:34 -0700

    dist => docs

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     mode: 'production',
     output: {
         filename: 'js/[name].[contentHash].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     optimization: {
         minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
